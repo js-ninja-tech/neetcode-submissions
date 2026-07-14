9917245e88575e99d94e91f858686f3f64ff4f4f
js-ninja-tech <122862532+js-ninja-tech@users.noreply.github.com>
2026-07-14 17:48:27 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,62 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length != t.length) {
+            return false;
+        }
+
+        let sMap = new Map();
+
+
+        for (const ch of s) {
+            sMap.set(ch, (sMap.get(ch) ?? 0) + 1);
+        }
+        for(let ch of t) {
+            if(!sMap.has(ch)) {
+                return false;
+            }
+
+            const count = sMap.get(ch) - 1;
+            if(count == 0) {
+                sMap.delete(ch);
+            } else {
+                sMap.set(ch, count)
+            }
+        }
+        return sMap.size === 0;
+
+
+        // let tMap = new Map();
+        // for (let i of s) {
+        //     if (sMap.has(i)) {
+        //         sMap.set(i, sMap.get(i) + 1);
+        //     } else {
+        //         sMap.set(i, 1);
+        //     }
+        // }
+
+        // for (let i of t) {
+        //     if (tMap.has(i)) {
+        //         tMap.set(i, tMap.get(i) + 1);
+        //     } else {
+        //         tMap.set(i, 1);
+        //     }
+        // }
+
+        // for (const [key, value] of sMap) {
+        //     if(!tMap.has(key)) {
+        //         return false;
+        //     }
+
+        //     const tMapValue = tMap.get(key);
+        //     if (tMapValue !== value) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+    }
+}
