bec9b7b5093b7d2b37ff8724b52c40988e5d89fe
js-ninja-tech <122862532+js-ninja-tech@users.noreply.github.com>
2026-08-09 06:40:01 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        let prefix = [];
+        let suffix = [];
+        let result = [];
+        prefix[0] = 1;
+        for(let i =1; i< nums.length; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1]
+        }   
+        suffix[nums.length -1] = 1;
+
+        for(let i =nums.length -2; i >= 0; i--) {
+            suffix[i] = suffix[i+1] * nums[i+1]
+        }
+
+        for(let i=0; i < nums.length; i++) {
+            result[i] = prefix[i] * suffix[i]
+        }
+
+        return result;
+    }
+}
